191439f6ab8c969704e424ad33719c942e41623b
Smith Orcale <08232644@cumt.edu.cn>
2025-04-15 21:46:31 +0800

    完成了filter和interceptor，该版本保留了interceptor拦截逻辑，filter逻辑保留

---
+++ src/main/java/org/example/tails/configuration/WebConfig.java
@@ -0,0 +1,20 @@
+package org.example.tails.configuration;
+
+import org.example.tails.interceptor.TokenInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    //拦截器对象
+    @Autowired
+    private TokenInterceptor tokenInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        //注册自定义拦截器对象
+        registry.addInterceptor(tokenInterceptor).addPathPatterns("/**");
+    }
+}
